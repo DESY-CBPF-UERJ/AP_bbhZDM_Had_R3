a8a8d98bba615bb80df4b75a3a60f9e85c688e15
MataiasCost <37384431+MataiasCost@users.noreply.github.com>
2025-11-17 18:52:01 -0300

    Update Bkg.py
    Aplicando uma correção

--- Datasets/Files/Bkg.py
+++ Datasets/Files/Bkg.py
@@ -6,7 +6,7 @@ campaigns["12_0_22"] = "Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5"
 campaigns["12_1_22"] = "Run3Summer22EENanoAODv12-130X_mcRun3_2022_realistic_postEE_v6"
 campaigns["12_0_23"] = "Run3Summer23NanoAODv12-130X_mcRun3_2023_realistic_v14"
 campaigns["12_1_23"] = "Run3Summer23BPixNanoAODv12-130X_mcRun3_2023_realistic_postBPix_v2"
-
+campaigns["15_0_24"] = "RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2"
 
 #==================================================================================================
 # Datasets setup
