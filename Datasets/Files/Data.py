845bdb4bdecd88385dc63fbdd68f7b3f71ed4c36
MataiasCost <37384431+MataiasCost@users.noreply.github.com>
2025-11-24 14:55:34 -0300

    Update Data.py
    Incluido parte da informação de 2024 (falta incluir a companha)

--- Datasets/Files/Data.py
+++ Datasets/Files/Data.py
@@ -4,7 +4,7 @@
 eras = {}
 eras["22"] = ['C', 'D', 'E', 'F', 'G']
 eras["23"] = ['C', 'D']
-
+eras["24"] = ['C','D','E','F','G','H','I']
 
 #==================================================================================================
 # Campaigns setup  (key = version_year_era)
@@ -13,18 +13,24 @@ campaigns = {}
 for era in set(sum(eras.values(), [])):
     campaigns["12_22_"+era] = "CMSSW_13_2_0-130X_dataRun3_v2_RelVal_2022"+era
     #campaigns["12_23_"+era] = "CMSSW_13_2_0-130X_dataRun3_v2_RelVal_2022"+era
+    campaigns["15_24_"+era] = "CMSSW_13_2_0-130X_dataRun3_v2_RelVal_2022"+era
 
 
 #==================================================================================================
 # Datasets setup  (key = year)
 #==================================================================================================
 datasets = {}
-datasets["22"] = [
-["Data_SingleMu",      "/SingleMuon/"],
-["Data_SingleEle",     "/SingleElectron/"],
-]
+#datasets["22"] = [
+#["Data_SingleMu",      "/SingleMuon/"],
+#["Data_SingleEle",     "/SingleElectron/"],
+#]
 
 #datasets["23"] = [
 #["Data_SingleMu",      "/SingleMuon/"],
 #["Data_SingleEle",     "/SingleElectron/"],
 #]
+
+datasets["24"] = [
+["Data_SingleMu",      "/SingleMuon/"],
+["Data_SingleEle",     "/SingleElectron/"],
+]
