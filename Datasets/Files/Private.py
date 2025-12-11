41840a880fb6b5e414126917fcda6d7b4de6faa6
MataiasCost <37384431+MataiasCost@users.noreply.github.com>
2025-12-11 15:14:26 -0300

    Update Private.py
    Inclui o de 2024. Vou usar isso pra confirmar algumas mudanças. Talvez eu tenh que voltar essa mudança

--- Datasets/Files/Private.py
+++ Datasets/Files/Private.py
@@ -10,3 +10,11 @@ datasets["0_22"] = [
 ["Signal_A800_a400",    "/eos/user/m/mlopescr/Miguel_Project_2HDM/EXO-Run3Summer22_bbA_ha_qqChiChi/H800_a400"],
 ["Signal_A1000_a100",   "/eos/user/m/mlopescr/Miguel_Project_2HDM/EXO-Run3Summer22_bbA_ha_qqChiChi/H1000_a100"],
 ]
+datasets["0_24"] = [
+["Signal_H400_a100",    "/eos/user/m/mlopescr/Miguel_Project_2HDM/EXO-Run3Summer22/H400_a100"],
+["Signal_H800_a400",    "/eos/user/m/mlopescr/Miguel_Project_2HDM/EXO-Run3Summer22/H800_a400"],
+["Signal_H1000_a100",   "/eos/user/m/mlopescr/Miguel_Project_2HDM/EXO-Run3Summer22/H1000_a100"],
+["Signal_A400_a100",    "/eos/user/m/mlopescr/Miguel_Project_2HDM/EXO-Run3Summer22_bbA_ha_qqChiChi/H400_a100"],
+["Signal_A800_a400",    "/eos/user/m/mlopescr/Miguel_Project_2HDM/EXO-Run3Summer22_bbA_ha_qqChiChi/H800_a400"],
+["Signal_A1000_a100",   "/eos/user/m/mlopescr/Miguel_Project_2HDM/EXO-Run3Summer22_bbA_ha_qqChiChi/H1000_a100"],
+]
